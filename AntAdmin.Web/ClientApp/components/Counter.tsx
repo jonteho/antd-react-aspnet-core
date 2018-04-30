f547425841969c8c9e669022ac3aa7bd5f8bd7c7
Jonathan Holm <jonathan.holm@omikai.com>
2018-04-30 13:54:15 +0200

    Test modal in counter

--- AntAdmin.Web/ClientApp/components/Counter.tsx
+++ AntAdmin.Web/ClientApp/components/Counter.tsx
@@ -1,20 +1,49 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Button, Badge } from 'antd';
+import { Button, Badge, Modal } from 'antd';
 const ButtonGroup = Button.Group;
 
 interface CounterState {
     currentCount: number;
+    modalVisible: boolean;
 }
 
 export class Counter extends React.Component<RouteComponentProps<{}>, CounterState> {
     constructor(props: any) {
         super(props);
-        this.state = { currentCount: 0 };
+        this.state = { 
+            currentCount: 0 ,
+            modalVisible: false
+        };
+    }
+    showModal = () => {
+        this.setState({
+            modalVisible: true,
+        });
+    }
+    handleOk = (e: any) => {
+        console.log(e);
+        this.setState({
+            modalVisible: false,
+        });
+    }
+    handleCancel = (e: any) => {
+        console.log(e);
+        this.setState({
+            modalVisible: false,
+        });
     }
 
     public render() {
         return <div>
+            <Button type="primary" onClick={this.showModal}>Show counter</Button>
+            <Modal
+                title="My Modal"
+                visible={this.state.modalVisible}
+                onOk={this.handleOk}
+                onCancel={this.handleCancel}
+            >
+            <div>
             <h1>Counter <span><Badge style={{ marginLeft: 10, backgroundColor: '#52c41a' }} count={ this.state.currentCount }>
                 <a href="#" className="head-example" />
             </Badge>
@@ -31,6 +60,8 @@ export class Counter extends React.Component<RouteComponentProps<{}>, CounterSta
                     <Button onClick={ () => { this.decreaseCounter() } }>Decrease</Button>
                 </ButtonGroup>
             </div>
+        </div>
+            </Modal>
         </div>;
     }
 

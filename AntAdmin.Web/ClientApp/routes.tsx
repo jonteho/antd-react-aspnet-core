64db078c28a63f84c7369bb1af10e3d9f38d8cf6
Jonathan Holm <jonathan.holm@hotmail.com>
2018-05-01 12:25:06 +0200

    routes

--- AntAdmin.Web/ClientApp/routes.tsx
+++ AntAdmin.Web/ClientApp/routes.tsx
@@ -5,10 +5,12 @@ import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 import { Login } from './components/Login';
+import { Cards } from './components/Cards';
 
 export const routes = <AppLayout>
     <Route exact path='/' component={ Home } />
     <Route path='/counter' component={ Counter } />
     <Route path='/fetchdata' component={ FetchData } />
     <Route path='/login' component={ Login } />
+    <Route path='/cards' component={ Cards } />
 </AppLayout>;
